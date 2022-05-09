72435c14c19ac3bba475358ea13161dbbc8142f8
Parham Alvani <parham.alvani@gmail.com>
2022-05-09 18:28:05 +0430

    feat: add logger and configuration

--- cmd/students/main.go
+++ cmd/students/main.go
@@ -4,20 +4,31 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"githuh.com/cng-by-example/students/internal/config"
 	"githuh.com/cng-by-example/students/internal/http/handler"
 	"githuh.com/cng-by-example/students/internal/store/student"
+	"go.uber.org/zap"
 )
 
 func main() {
+	cfg := config.New()
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	app := fiber.New()
 
 	hnd := handler.Student{
-		Store: student.NewInMemory(),
+		Store:  student.NewInMemory(),
+		Logger: logger.Named("handler.student"),
 	}
 
 	hnd.Register(app.Group("/students"))
 
-	if err := app.Listen(":1373"); err != nil {
-		log.Fatal(err)
+	if err := app.Listen(cfg.Listen); err != nil {
+		logger.Fatal("cannot listen", zap.String("listen", cfg.Listen),
+			zap.Error(err))
 	}
 }
